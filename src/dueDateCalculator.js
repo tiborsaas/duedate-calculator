848ff0acf0d68b30c205f0190202d988057ec14c
Tibor Szasz <tszasz@gmail.com>
2018-10-04 14:24:54 +0200

    chore(calc): add solution plan

--- src/dueDateCalculator.js
+++ src/dueDateCalculator.js
@@ -1,6 +1,14 @@
 /**
  * Epam homework assignment
  */
-function dueDateCalculator(dateCreated, turnaroundTime) { }
+function dueDateCalculator(dateCreated, turnaroundTime) {
+    // solution plan:
+
+    // convert issue creation to ts
+    // get day units from diffrence of turnaroundTime
+    // get due date by multiplying diff and seconds in a day
+    // get number of weekends in given timeframe, add that to end date timestamp
+    // convert timestamp back to datestring
+}
 
 module.exports = dueDateCalculator;
