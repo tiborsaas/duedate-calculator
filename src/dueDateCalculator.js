f1176b0de249b7c25bc99d57a283d4f165118993
Tibor Szasz <tszasz@gmail.com>
2018-10-05 15:18:59 +0200

    feat(main): tests are passing with basic algo

--- src/dueDateCalculator.js
+++ src/dueDateCalculator.js
@@ -1,3 +1,5 @@
+const dateLib = require('./helpers');
+
 /**
  * Epam homework assignment
  */
@@ -11,7 +13,17 @@ function dueDateCalculator(dateCreated, turnaroundTime) {
     //  - if we are on workdays
     //      - decrease turnaround time
     //  - otherwise keep iterating in time
+    const timeline = new Date(dateCreated);
 
+    let remainingTime = turnaroundTime * 60;
+
+    while (remainingTime > 0) {
+        if (timeline.getUTCHours() >= 9 && timeline.getUTCHours() < 17 && timeline.getDay() !== 0 && timeline.getDay() !== 6 ) {
+            remainingTime--;
+        }
+        timeline.setTime(timeline.getTime() + 1000 * 60);
+    }
+    return dateLib.timestampToDate(timeline.getTime());
 }
 
 module.exports = dueDateCalculator;
