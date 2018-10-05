93860f6958a903afba8b24bc148d5a9fb206d6db
Tibor Szasz <tszasz@gmail.com>
2018-10-05 16:06:41 +0200

    feat(main): add basic usage example

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 const calculator = require('./src/dueDateCalculator');
+const dateLib = require('./src/helpers');
 
-console.log(calculator('2018-10-04T12:00:00.000Z', 4));
+const issueStartDate = '2018-10-04T12:00:00.000Z';
+const allocatedTime = 4;
+
+const dueDate = calculator(issueStartDate, allocatedTime);
+
+const issueDateObj = new Date(issueStartDate);
+const formattedDate = dateLib.timestampToDate(issueDateObj.getTime());
+
+console.log(`The issue created at ${formattedDate} and with ${allocatedTime} allocated hours should be done until ${dueDate}`);
