ac314440af999b4983af52b8d876f49127ed1ede
Tibor Szasz <tszasz@gmail.com>
2018-10-05 15:40:40 +0200

    refactor(main): move magic numbers to constants and add a small comment

--- src/dueDateCalculator.js
+++ src/dueDateCalculator.js
@@ -1,7 +1,13 @@
 const dateLib = require('./helpers');
 
+const WORK_START_TIME = 9;
+const WORK_END_TIME = 17;
+const WEEKENDS = [0, 6];
+
 /**
- * Epam homework assignment
+ * Calculate a future event date based on time allocated work time
+ * @param {string} dateCreated: a parsable datestring
+ * @param {number} turnaroundTime: work time dedicated to task in hours
  */
 function dueDateCalculator(dateCreated, turnaroundTime) {
     if (turnaroundTime <= 0) {
@@ -15,7 +21,10 @@ function dueDateCalculator(dateCreated, turnaroundTime) {
     let remainingTime = turnaroundTime * 60;
 
     while (remainingTime > 0) {
-        if (timeline.getUTCHours() >= 9 && timeline.getUTCHours() < 17 && timeline.getDay() !== 0 && timeline.getDay() !== 6) {
+        const hour = timeline.getUTCHours();
+        const day = timeline.getDay();
+
+        if (hour >= WORK_START_TIME && hour < WORK_END_TIME && !WEEKENDS.includes(day)) {
             remainingTime--;
         }
         timeline.setTime(timeline.getTime() + 1000 * 60);
