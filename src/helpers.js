5c11b87df4bc760f73946f0e6d975cda4b5b4327
Tibor Szasz <tszasz@gmail.com>
2018-10-05 15:32:14 +0200

    refactor(helpers): remove most functions

--- src/helpers.js
+++ src/helpers.js
@@ -8,16 +8,6 @@ function zeroPad(number) {
     return number < 10 ? `0${numAsString}` : numAsString;
 }
 
-function getWeekOfDate(timestamp) {
-    const date = new Date(timestamp);
-    const januaryFirst = new Date(date.getFullYear(), 0, 1);
-    return Math.ceil((((date - januaryFirst) / MILLISECONDS_IN_DAY) + januaryFirst.getDay() + 1) / 7);
-}
-
-function dateToTimestamp(dateTime) {
-    return new Date(dateTime).getTime();
-}
-
 function timestampToDate(timestamp) {
     if (timestamp < 0 || typeof timestamp !== 'number') {
         return false;
@@ -31,39 +21,7 @@ function timestampToDate(timestamp) {
     return `${year}-${month}-${day} ${hour}:${minutes}`;
 }
 
-function getElapsedWorkHourInDay(timestamp) {
-    const date = new Date(timestamp);
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    const day = date.getDate();
-    // 9am that day
-    const startOfDay = new Date(year, month, day, 9, 0).getTime();
-    return (timestamp - startOfDay) / 1000 / 60 / 60;
-}
-
-function workHoursElapsedInWeek(timestamp) {
-    const date = new Date(timestamp);
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    const day = date.getDate();
-
-    const monday = day - (date.getDay() - 1);
-    const startOfWeek = new Date(year, month, monday, 9, 0);
-    let hourcount = 0;
-
-    while (startOfWeek.getTime() < timestamp) {
-        if (startOfWeek.getHours() >= 9 && startOfWeek.getHours() < 17) {
-            hourcount++;
-        }
-        startOfWeek.setTime(startOfWeek.getTime() + 1000 * 60);
-    }
-    return hourcount / 60;
-}
-
 module.exports = {
     zeroPad,
-    dateToTimestamp,
-    timestampToDate,
-    workHoursElapsedInWeek,
-    getElapsedWorkHourInDay
+    timestampToDate
 }
--- src/helpers.test.js
+++ src/helpers.test.js
@@ -9,23 +9,8 @@ test('Zero padding', () => {
     expect(helpers.zeroPad(1)).toBe('01');
 });
 
-test('Date to timestamp', () => {
-    expect(helpers.dateToTimestamp('2018-01-01 12:00')).toBe(1514804400000);
-    expect(helpers.dateToTimestamp('garbage')).toBeFalsy();
-});
-
 test('Timestamp to date', () => {
-    expect(helpers.timestampToDate(1514804400000)).toBe('2018-01-01 12:00');
+    expect(helpers.timestampToDate(1538745716380)).toBe('2018-10-05 13:21');
     expect(helpers.timestampToDate(-1248710)).toBeFalsy();
     expect(helpers.timestampToDate('garbage')).toBeFalsy();
-});
-
-test('Elapsed hours in day', () => {
-    expect(helpers.getElapsedWorkHourInDay(1514804400000)).toBe(3);
-    expect(helpers.getElapsedWorkHourInDay(1539500400000)).toBe(0);
-});
-
-test('Work hours elapsed in week', () => {
-    expect(helpers.workHoursElapsedInWeek(1539068400000)).toBe(8);
-    expect(helpers.workHoursElapsedInWeek(1539082800000)).toBe(12);
 });
