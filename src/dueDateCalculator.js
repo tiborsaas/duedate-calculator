5ca2efbc7cca83f74ac0346711982ad3031a8c90
Tibor Szasz <tszasz@gmail.com>
2018-10-05 15:33:04 +0200

    refactor(main): cleanup function and add error checks

--- src/dueDateCalculator.js
+++ src/dueDateCalculator.js
@@ -4,21 +4,18 @@ const dateLib = require('./helpers');
  * Epam homework assignment
  */
 function dueDateCalculator(dateCreated, turnaroundTime) {
-    // solution plan:
-
-    // the program needs to decrease turnaroundTime to zero:
-    //
-    // start counting from the dateCreated
-    //  - if we are between busicness hours
-    //  - if we are on workdays
-    //      - decrease turnaround time
-    //  - otherwise keep iterating in time
+    if (turnaroundTime <= 0) {
+        throw new TypeError('The turnaround time should be larger than zero');
+    }
     const timeline = new Date(dateCreated);
+    if (timeline === 'Invalid date') {
+        throw new TypeError('Issue creation date is invalid: ' + dateCreated);
+    }
 
     let remainingTime = turnaroundTime * 60;
 
     while (remainingTime > 0) {
-        if (timeline.getUTCHours() >= 9 && timeline.getUTCHours() < 17 && timeline.getDay() !== 0 && timeline.getDay() !== 6 ) {
+        if (timeline.getUTCHours() >= 9 && timeline.getUTCHours() < 17 && timeline.getDay() !== 0 && timeline.getDay() !== 6) {
             remainingTime--;
         }
         timeline.setTime(timeline.getTime() + 1000 * 60);
